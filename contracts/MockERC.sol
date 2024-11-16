d1d7047ce739fbfc695c682218955a8370e1b2c3
eliashezron <eliashezron23@gmail.com>
2024-11-16 18:41:01 +0700

    add networks

---
+++ contracts/MockERC.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    uint256 public constant MINT_AMOUNT = 10000 * (10 ** 18);
+
+    constructor() ERC20("Mock USDC", "mUSDC") {} 
+
+    // Function to mint tokens to the caller
+    function mint() external {
+        _mint(msg.sender, MINT_AMOUNT);
+    }
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,57 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const accounts = [
+	process.env.PRIVATE_KEY
+].filter((key): key is string => !!key);
 
 const config: HardhatUserConfig = {
+  networks: {		
+		"ethereum-testnet": {
+			chainId: 11155111,
+			url: "https://eth-sepolia.public.blastapi.io",
+			accounts: accounts,
+		},
+		"unichain-testnet": {
+			chainId: 1301,
+			url: "https://sepolia.unichain.org",
+			accounts: accounts,
+		},
+		"scroll-testnet": {
+			chainId: 534351,
+			url: "https://scroll-sepolia.chainstacklabs.com",
+			accounts: accounts,
+		},
+		alfajores: {
+			url: "https://alfajores-forno.celo-testnet.org",
+			accounts: accounts,
+			chainId: 44787,
+		  },
+	},
+	etherscan: {
+		apiKey: {
+		  // Is not required by blockscout. Can be any non-empty string
+		  'alfajores': "abc"
+		},
+		customChains: [
+		  {
+			network: "alfajores",
+			chainId: 11155420,
+			urls: {
+			  apiURL: "https://optimism-sepolia.blockscout.com/api",
+			  browserURL: "https://optimism-sepolia.blockscout.com/",
+			}
+		  }
+		]
+	  },
+	  sourcify: {
+		enabled: false
+	  },
   solidity: {
     compilers: [
       {
